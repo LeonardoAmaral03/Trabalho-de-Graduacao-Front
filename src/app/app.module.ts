6eb82911a0b8939e269c9547dda3d93f91968f84
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-08-10 19:38:30 -0300

    Aplicação Criada. Componentes de manutenção criados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaintenancesComponent } from './maintenance/maintenances/maintenances.component';
+import { MaintenanceDetailComponent } from './maintenance/maintenance-detail/maintenance-detail.component';
+import { MaintenanceNewComponent } from './maintenance/maintenance-new/maintenance-new.component';
+import { MaintenanceEditComponent } from './maintenance/maintenance-edit/maintenance-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaintenancesComponent,
+    MaintenanceDetailComponent,
+    MaintenanceNewComponent,
+    MaintenanceEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maintenance/maintenance-edit/maintenance-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-edit',
+  templateUrl: './maintenance-edit.component.html',
+  styleUrls: ['./maintenance-edit.component.scss']
+})
+export class MaintenanceEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
