029ffb53fbff94374470d26debcfbf77661e9616
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-08-10 20:09:25 -0300

    Configurando rotas dos componentes de manutenção

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaintenancesComponent } from './maintenance/maintenances/maintenances.component';
+import { MaintenanceDetailComponent } from './maintenance/maintenance-detail/maintenance-detail.component';
+import { MaintenanceNewComponent } from './maintenance/maintenance-new/maintenance-new.component';
+import { MaintenanceEditComponent } from './maintenance/maintenance-edit/maintenance-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'maintenances',
+    component: MaintenancesComponent,
+    data: { title: 'Maintenance List' }
+  },
+  {
+    path: 'maintenance-detail/:id',
+    component: MaintenanceDetailComponent,
+    data: { title: 'Maintenance Detail' }
+  },
+  {
+    path: 'maintenance-new',
+    component: MaintenanceNewComponent,
+    data: { title: 'Add Maintenance' }
+  },
+  {
+    path: 'maintenance-edit/:id',
+    component: MaintenanceEditComponent,
+    data: { title: 'Edit Maintenance' }
+  },
+  {
+    path: '',
+    redirectTo: '/maintenances',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
