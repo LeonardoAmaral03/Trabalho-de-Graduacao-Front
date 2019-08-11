92bd36a639506a84dac36721336f12b956107523
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-08-11 13:35:12 -0300

    Criado menu lateral com angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintenancesComponent } from './maintenance/maintenances/maintenances.component';
 import { MaintenanceDetailComponent } from './maintenance/maintenance-detail/maintenance-detail.component';
 import { MaintenanceNewComponent } from './maintenance/maintenance-new/maintenance-new.component';
 import { MaintenanceEditComponent } from './maintenance/maintenance-edit/maintenance-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './shared/menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -14,11 +29,27 @@ import { MaintenanceEditComponent } from './maintenance/maintenance-edit/mainten
     MaintenancesComponent,
     MaintenanceDetailComponent,
     MaintenanceNewComponent,
-    MaintenanceEditComponent
+    MaintenanceEditComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
