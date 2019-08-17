70bfa4fe955ca4f0b719aca920b10dd7a34e43fe
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-08-17 16:55:10 -0300

    Inserido componente para cadastrar nova manutenção

--- src/app/maintenance/maintenance-new/maintenance-new.component.ts
+++ src/app/maintenance/maintenance-new/maintenance-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiMaintenanceService } from '../../../services/api-maintenance.service';
 
 @Component({
   selector: 'app-maintenance-new',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintenanceNewComponent implements OnInit {
 
-  constructor() { }
+  maintenanceForm: FormGroup;
+  isLoadingResults = false;
+
+  constructor(private router: Router, private api: ApiMaintenanceService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.maintenanceForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      activity : [null, [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  addMaintenance(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addMaintenance(form)
+      .subscribe(res => {
+          const id = res.id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/manutenção-detalhe', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
   }
 
 }
